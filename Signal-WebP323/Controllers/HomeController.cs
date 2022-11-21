0af436648d68212f4b916d525118af40ce72aa41
Mirsaleh <mirsalehaa@code.edu.az>
2022-11-22 01:46:25 +0400

    WebSocket,SignalR

---
+++ Signal-WebP323/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Signal_WebP323.Models;
+using System.Diagnostics;
+
+namespace Signal_WebP323.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public HomeController()
+        { 
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Signal-WebP323/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Signal_WebP323.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task AddTogroup(string group) 
+        {
+           await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task RemoveFromGroup(string group) 
+        {
+           await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
+        
+        }
+        public async Task SendMessage(string user, string group, string message)
+        {
+            await Clients.Group(group).SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
